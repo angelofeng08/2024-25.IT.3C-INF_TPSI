3791e1884c240afb6ea70a29adde0199b8c7da63
angelofeng08 <angelofeng157@gmail.com>
2024-12-18 00:03:34 +0000

    MODIFICHE 6

--- hello.c
+++ hello.c
@@ -17,7 +17,7 @@ int AND(int a, int B) {
 int OR(int a, int B) {
     // Il valore di ritorno della funzione
     // TODO aggiungere l'espressione matematica per soddisfare la porta OR
-    return (-((-a+1)*(-B+1))+1);
+    return a + B - (a*B);
 }
 
 int main() {
@@ -64,19 +64,23 @@ int main() {
     // Mostra un messaggio sul terminale che l'utente può leggere
     printf("Hai inserito il valore F: %d\n", F);
 
-    if (A != 0 && A != 1 && B != 0 && B != 1 && A != 0 && A != 1 && B != 0 && B != 1 && A != 0 && A != 1 && B != 0 && B != 1) {
+    if ((A != 0 && A != 1) || (B != 0 && B != 1) || (C != 0 && C != 1) ||
+        (D != 0 && D != 1) || (E != 0 && E != 1) || (F != 0 && F != 1))  {
         printf("I valori inseriti non sono 1 o 0\n");
-    } else {
+    }
+    else {
         // Procediamo con l'esecuzione solo se il numero è 1 o 0
         printf("I valori inseriti sono 1 o 0\n");
-
+        int R1= AND(B, C);
+        int R2= OR(NOT(A), A);
+        int R3= AND(D, E);
+        int R4= OR(NOT(R3), F);
+        int R5= OR(R1, R2);
+        int FINALE= OR(R4,R5);
         
-        
-        int R = OR((OR(NOT(A), A)), B,AND(C, D),E,F);
-        printf("Il valore di uscita della porta NOT è: %d\n", R);
+        printf("Il valore di uscita della porta NOT è: %d\n", FINALE);
 
-        int Bouns
-    }
+      
 
     printf("\n\n");
 }
